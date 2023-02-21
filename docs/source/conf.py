908288406fd8d336de285564d47f40fedb3477ed
Frenz Jay-r Y. Valios <67408909+frenzjay@users.noreply.github.com>
2023-02-21 18:13:12 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'FEQAB - MB'
+copyright = '2023, frenzvalios.tk'
+author = 'Frenz Valios'
 
 release = '0.1'
 version = '0.1.0'
